11ebfb6a904ec4de2d6f90228f7103f4f6a4cc43
Joel Ostos <66744530+Joel-Ostos@users.noreply.github.com>
2022-04-30 01:26:34 +0200

    Corrigiendo prueba

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from logica.init_db  import conexion_a_la_db, eliminar_tablas, crear_tablas
 from logica.modulo_1 import crear_equipo
+
 from presentacion.modulo_1 import consultar_equipo_por_id_user, crear_equipo_user
+
+from logica.modulo_3
+from presentacion.modulo_3
 
 #Inicializar base de datos
 conexion = conexion_a_la_db()
 #   Eliminar esta línea cuando se hallan acabado las pruebas
 eliminar_tablas(conexion)
 crear_tablas(conexion)
+
 
 crear_equipo_user(conexion)
 consultar_equipo_por_id_user(conexion)
