1d812264514d160c28e9d122cdf87d565aca7361
AndrewF35 <47949583+AndrewF35@users.noreply.github.com>
2022-05-01 21:46:09 -0500

    Actualización Módulo 4

--- logica/modulo_4.py
+++ logica/modulo_4.py
@@ -1,17 +1,42 @@
 # Módulo 4: Consulta de clasificaciones
-
-from logica.init_db  import conexion_a_la_db
-
-conexion = conexion_a_la_db()
-
 #Consulta individual por etapa
 
-def consulta_ciclista_ID_etapa(conexion,ID):
+
+
+def consulta_etapa(conexion,etapa,orden):
 	'''
-	Función consulta individual por etapa.
+	Función consulta por etapa.
 	Recibe un objeto conexión.
-	Recibe la identificación única del ciclista.
-	Consulta la informacion de un ciclista mediante su ID única para una etapa determinada
+	Recibe el parámetro de orden de tabla.
+	Consulta la informacion de la clasificación de la etapa, ordenada según el parámetro recibido
 	'''
+	#Falta hacer la verificación de el parámetro orden en la capa de presentación
 	cursor = conexion.cursor()
-	
+	sentencia_consulta = f'''SELECT clasificacion.num_etapa,
+	ciclistas.num_inscripcion_ciclista,
+	ciclistas.nombre,
+	ciclistas.apellido,
+	ciclistas.pais,
+	equipos.nombre AS nombre_equipo,
+	clasificacion.tiempo_empleado
+	FROM ciclistas 
+	INNER JOIN equipos ON  ciclistas.num_equipo = equipos.num_equipo 
+	INNER JOIN clasificacion ON ciclistas.num_inscripcion_ciclista = clasificacion.num_ciclista AND clasificacion.num_etapa = {etapa}
+	ORDER BY {orden}'''
+	consulta = cursor.execute(sentencia_consulta)
+
+	return consulta
+
+def consulta_general(conexion,orden):
+	'''
+	Función consulta de la clasificación general.
+	Recibe un objeto conexión.
+	Recibe el parámetro de orden de la tabla.
+	Consulta la informacion de la clasificación general de la carrera, ordenada según el parámetro recibido
+	'''
+
+	cursor = conexion.cursor()
+	sentencia_consulta = f'''
+	'''
+
+	return
---
+++ presentacion/modulo_4.py
@@ -0,0 +1,4 @@
+from terminaltables import AsciiTable
+from logica.modulo_4 import consulta_etapa
+
+
