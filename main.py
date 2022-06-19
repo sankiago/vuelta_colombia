f731ad28527df995b892198e894af088fa09d994
lpzsds <lpzsds@gmail.com>
2022-06-18 23:01:43 -0500

    estableciendo estructura eel e implementando vista home

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from logica.init_db        import conexion_a_la_db, eliminar_tablas, crear_tablas
-from presentacion.menu     import mostrar_menu
+from   logica.init_db        import conexion_a_la_db, eliminar_tablas, crear_tablas
+from   logica                import *
+import eel
 
 #Inicializar base de datos
 conexion = conexion_a_la_db('base_de_datos_vc.db')
-
 crear_tablas(conexion)
 
-mostrar_menu(conexion)
+#Exponiendo funciones a js
+EquipoDAO
+CiclistaDAO
+EtapaDAO
+Resultado_DAO
+
+#Inicializar conexión js/python
+eel.init('client')
+eel.start('home.html')
+
+
+
+
 
 
 
